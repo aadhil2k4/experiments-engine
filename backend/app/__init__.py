8d438896e44e2bbb8e8e63711917555a5f67b0a9
poornimaramesh <poornimaramesh1995@gmail.com>
2025-05-08 21:54:44 +0300

    fix hot reload for backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from redis import asyncio as aioredis
 
 from . import auth, bayes_ab, contextual_mab, mab, messages
-from .config import DOMAIN, REDIS_HOST
+from .config import BACKEND_ROOT_PATH, DOMAIN, REDIS_HOST
 from .users.routers import (
     router as users_router,
 )  # to avoid circular imports
@@ -34,7 +34,11 @@ def create_app() -> FastAPI:
     """
     Create a FastAPI application with the experiments router.
     """
-    app = FastAPI(title="Experiments API", lifespan=lifespan)
+    app = FastAPI(
+        title="Experiments API",
+        lifespan=lifespan,
+        openapi_prefix=BACKEND_ROOT_PATH,
+    )
 
     origins = [
         f"http://{DOMAIN}",
